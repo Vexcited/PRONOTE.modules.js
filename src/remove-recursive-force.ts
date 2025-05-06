89c08ff5b562cffd12a65a99f246a4a1d885d0a8
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-05-07 01:03:27 +0200

    feat(utils): rm -rf command

---
+++ src/remove-recursive-force.ts
@@ -0,0 +1,14 @@
+import fs from "node:fs/promises";
+
+export async function removeRecursiveForce (path: string): Promise<void> {
+  try {
+    await fs.rm(path, { recursive: true, force: true });
+  }
+  catch (error) {
+    if ((error as NodeJS.ErrnoException).code === "ENOENT") {
+      return;
+    }
+
+    throw error;
+  }
+}
