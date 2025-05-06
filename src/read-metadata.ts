08c6b8bb69965fca0f4da4470c46f85946b190e3
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-05-07 01:03:39 +0200

    feat: read metadata from pronote.net app

---
+++ src/read-metadata.ts
@@ -0,0 +1,26 @@
+import { readFile } from 'node:fs/promises';
+
+export async function readVersionFromMetadata (path: string): Promise<string> {
+  const content = await readFile(path, 'utf16le');
+  const lines = content.split('\r\n');
+  console.log(lines)
+
+  // e.g.: `VALUE "ProductVersion",    "2024.3.11.0"`
+  let versionLine = lines.find(line => line.includes('VALUE "ProductVersion",'));
+  if (!versionLine) {
+    throw new Error('Version line not found in metadata');
+  }
+
+  // Remove all spaces.
+  // e.g: `VALUE"ProductVersion","2024.3.11.0"`
+  versionLine = versionLine.replaceAll(" ", "");
+
+  // e.g: `"2024.3.11.0"`
+  const versionWithQuotes = versionLine.split(',')[1];
+
+  // Remove quotes.
+  // e.g: `2024.3.11.0`
+  const version = versionWithQuotes.replaceAll('"', '');
+
+  return version;
+}
