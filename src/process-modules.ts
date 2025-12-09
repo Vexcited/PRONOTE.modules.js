1c422eb18710cef742a4aa336bf512b9fc31f04a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-12-09 20:20:51 +0100

    fix: css imports breaking formatting

--- src/process-modules.ts
+++ src/process-modules.ts
@@ -1,62 +1,58 @@
 import fs from "node:fs/promises";
-import { Biome, Distribution } from "@biomejs/js-api";
+import { Biome } from "@biomejs/js-api/nodejs";
 import { removeRecursiveForce } from "./remove-recursive-force";
 
 const EXPORT_REGEX = /},fn:(['"]).*?\1}\);/;
 const REQUIRE_FILENAME_REGEX = /require\((['"])(?<fileName>.*?)\1\);/g;
 const REQUIRE_CSS_REGEX = /require\((['"])([^'")]*?\.css)\1\)/g;
 
-const biome = await Biome.create({
-  distribution: Distribution.NODE,
-});
+const biome = new Biome();
 
 export async function processModules(
-  modulesDirectory: string,
+  modulesDirectory: string
 ): Promise<number> {
-  let time = performance.now();
   let files = await fs.readdir(modulesDirectory);
   files = files.filter((file) => file.endsWith("_JS"));
 
   await removeRecursiveForce("modules"); // Remove old modules directory if it exists.
   await fs.mkdir("modules/unused", { recursive: true });
 
+  const { projectKey } = biome.openProject("modules");
+
   const realFileNames: Record<string, string> = {};
   const modules: Record<string, string> = {};
 
   await Promise.all(
     files.map(async (fileName) => {
-      let rawContent = await fs.readFile(
-        `${modulesDirectory}/${fileName}`,
-        "utf8",
-      );
+      let raw = await Bun.file(`${modulesDirectory}/${fileName}`).text();
 
       // Remove every CSS imports.
-      rawContent = rawContent.replace(REQUIRE_CSS_REGEX, "");
+      raw = raw.replace(REQUIRE_CSS_REGEX, "{}");
 
       // Remove spaces at start and end.
-      rawContent = rawContent.trim();
+      raw = raw.trim();
 
       // Remove some wrapping from bundlers, useless for us.
-      rawContent = rawContent.replace(
+      raw = raw.replace(
         'Object.defineProperty(exports,"__esModule",{value:true});',
-        "",
+        ""
       );
 
-      if (rawContent.startsWith("IE.fModule")) {
+      if (raw.startsWith("IE.fModule")) {
         // e.g.: `IE.fModule({f:function(exports,require,module,global){` : 54 chars long
-        rawContent = rawContent.substring(54);
+        raw = raw.substring(54);
 
         // e.g.: `{ ... },fn:'_cache.js'});` // note the first { is removed from above.
-        rawContent = rawContent.replace(EXPORT_REGEX, "");
+        raw = raw.replace(EXPORT_REGEX, "");
       }
 
-      if (rawContent.startsWith('"use ')) {
+      if (raw.startsWith('"use ')) {
         // `"use strict;" : 13 chars long
-        rawContent = rawContent.substring(13);
+        raw = raw.substring(13);
       }
 
       // Get all JS imports and save the names to a map for later usage.
-      [...rawContent.matchAll(REQUIRE_FILENAME_REGEX)].forEach((match) => {
+      [...raw.matchAll(REQUIRE_FILENAME_REGEX)].forEach((match) => {
         let fileName = match.groups!.fileName;
         fileName = fileName.replace(/\.js$/, "");
         realFileNames[fileName.toLowerCase()] = fileName;
@@ -69,12 +65,12 @@ export async function processModules(
         .replace(/_JS$/, "")
         .toLowerCase();
 
-      const { content } = biome.formatContent(rawContent, {
+      const { content } = biome.formatContent(projectKey, raw, {
         filePath: fileName + ".js",
       });
 
       modules[fileName] = content;
-    }),
+    })
   );
 
   // Try to match all the lowercased file names with imports map.
@@ -86,9 +82,9 @@ export async function processModules(
         realFileName = "unused/" + lowercasedFileName;
       }
 
-      await fs.writeFile(`modules/${realFileName}.js`, content, "utf8");
+      await Bun.write(`modules/${realFileName}.js`, content);
       console.log(`+ ${lowercasedFileName}.js -> ${realFileName}.js`);
-    }),
+    })
   );
 
   return files.length;
